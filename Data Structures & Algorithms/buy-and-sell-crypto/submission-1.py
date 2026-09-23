9bcfcb286a1fd61d25fbe3458c7d9c4b70467fe3
Matvey Kolodin <99589603+HpPpL@users.noreply.github.com>
2026-09-23 18:03:15 +0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,14 @@
+# O(n) - time. O(1) - space.
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        minIndex, maxProfit = 0, 0
+
+        for i in range(1, n):
+            if maxProfit < prices[i] - prices[minIndex]:
+                maxProfit = prices[i] - prices[minIndex]
+
+            if prices[i] < prices[minIndex]:
+                minIndex = i 
+
+        return maxProfit
