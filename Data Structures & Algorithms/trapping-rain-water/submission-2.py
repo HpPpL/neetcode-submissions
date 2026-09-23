e63bc33cbf8ac72bb0e3fa786ae4e57544b3d2c1
Matvey Kolodin <99589603+HpPpL@users.noreply.github.com>
2026-09-23 13:35:36 +0300

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.py
@@ -0,0 +1,14 @@
+# O(n) - time. O(1) - space
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        leftMaxHeight, rightMaxHeight = [0] * n, [0] * n
+
+        for i in range(1, n):
+            leftMaxHeight[i] = max(leftMaxHeight[i-1], height[i-1])
+            rightMaxHeight[n-1-i] = max(rightMaxHeight[n-i], height[n-i])
+        
+        return sum(
+            max(0, min(leftMaxHeight[i], rightMaxHeight[i]) - tall)
+            for i, tall in enumerate(height)
+        )
