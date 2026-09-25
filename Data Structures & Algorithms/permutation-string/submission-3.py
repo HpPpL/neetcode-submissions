87a8a0945fcf3b261b22d4e34f51ae2546ca5b57
Matvey Kolodin <99589603+HpPpL@users.noreply.github.com>
2026-09-25 14:58:06 +0300

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,12 @@
+# O(n) - time. O(1) - space.
+from collections import Counter
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n, counts = len(s1), Counter(s1)
+        
+        for i in range(0, len(s2) - n + 1):
+            print(s2[i:i+n])
+            if Counter(s2[i:i+n]) == counts:
+                return True
+
+        return False
